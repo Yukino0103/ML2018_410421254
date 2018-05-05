2de20c109fd1411b55cd24cb1c6a530d38d9ebe5
Yukino0103 <410421254@gms.ndhu.edu.tw>
2018-05-05 23:02:53 +0800

    Update test.py

--- HW1/test.py
+++ HW1/test.py
@@ -24,20 +24,20 @@ test = np.zeros((300,400),int)
 #設定要輸出的矩陣大小
 
 for epoch in range(2):
-    for k in range(300):
-        for p in range(400):
-            a = np.dot(np.transpose(w),np.transpose(np.array([key1data[k , p], key2data[k , p], Idata[k , p]])))
-            e = Edata[k , p] - a
-            w = w + (0.00001 * e * np.transpose(np.array([key1data[k , p], key2data[k , p], Idata[k , p]])))
+    for i in range(300):
+        for j in range(400):
+            a = np.dot(np.transpose(w),np.transpose(np.array([key1data[i , j], key2data[i , j], Idata[i , j]])))
+            e = Edata[i , j] - a
+            w = w + (0.00001 * e * np.transpose(np.array([key1data[i , j], key2data[i , j], Idata[i , j]])))
 #利用Least Mean Square演算法,求出W1,W2,W3
 
-for k in range(300):
-    for p in range(400):
-        test[k , p] = (Epdata[k , p] - w[0] * key1data[k , p] - w[1] * key2data[k , p]) / w[2]
-        if test[k , p] > 255:
-            test[k , p] = 255
-        elif test[k , p] < 0:
-            test[k , p] = 0
+for i in range(300):
+    for j in range(400):
+        test[i , j] = (Epdata[i , j] - w[0] * key1data[i , j] - w[1] * key2data[i , j]) / w[2]
+        if test[i , j] > 255:
+            test[i , j] = 255
+        elif test[i , j] < 0:
+            test[i , j] = 0
 #利用解密公式得出解密圖
             
 
